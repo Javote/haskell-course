830c7dd5106509020228e3cbc1e1e38516306ce2
Javote <55247241+Javote@users.noreply.github.com>
2024-07-04 19:13:16 +0000

    Homework 04 completed.

--- Homework/Homework04/Homework04.hs
+++ Homework/Homework04/Homework04.hs
@@ -1,4 +1,3 @@
-import Distribution.Simple.Utils (xargs)
 -- Question 1
 -- Lets say you have the nested values defined bellow. How would you get the value of
 -- 4 by using only pattern matching in a function?
--- Homework/Homework05/Homework05.hs
+++ Homework/Homework05/Homework05.hs
@@ -2,25 +2,40 @@
 -- flips the order of the parameters.
 -- For example this: `(/) 6 2` returns `3`. But this: `flip' (/) 6 2` returns `0.3333333333`
 
+flipParameters :: (a -> b -> c) -> b -> a -> c
+flipParameters f x y = f y x
+
 
 -- Create the `uncurry'` function that converts a curried function to a function on pairs. So this: `(+) 1 2` that returns `3` can be written as
 -- `uncurry' (+) (1,2)` (with the two different arguments inside a pair).
 
+uncurryFunction :: (a -> b -> c) -> ((a, b) -> c)
+uncurryFunction f (x, y) = f x y
 
 -- Create the `curry'` function that converts an uncurried function to a curried function. So this: `fst (1,2)` that returns `1` can be written as
 -- `curry' fst 1 2` (with the tuple converted into two different arguments).
 
+curryFunction :: ((a, b) -> c) -> a -> b -> c
+curryFunction f x y = f (x, y)
 
 -- Use higher-order functions, partial application, and point-free style to create a function that checks if a word has an uppercase letter.
 -- Start with using just higher-order functions and build from there. 
 
+hasUppercase :: String -> Bool
+hasUppercase = any (`elem` ['A'..'Z'])
 
 -- Create the `count` function that takes a team ("Red", "Blue", or "Green") and returns the amount of votes the team has inside `votes`.
 
 votes :: [String]
 votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
 
+count :: String -> Int
+count x = length . filter (== x) $ votes 
+
 -- Create a one-line function that filters `cars` by brand and then checks if there are any left.
 
 cars :: [(String,Int)]
 cars = [("Toyota",0), ("Nissan",3), ("Ford",1)]
+
+anyCarsLeft :: String -> Bool
+anyCarsLeft x = any (\(_, cars) -> cars > 0) . filter (\(brand, _) -> brand == x) $ cars
