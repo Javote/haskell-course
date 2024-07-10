9d3be6b0baf726875ffdf726e119a2ff50dc448e
Javote <55247241+Javote@users.noreply.github.com>
2024-07-10 16:05:18 +0000

    Homework 09 completed.

--- Homework/Homework09/2-Forest.hs
+++ Homework/Homework09/2-Forest.hs
@@ -42,3 +42,55 @@ Using GHCi, like the "Maze" game, this game should look like this:
 *Main> solveForest testForest [GoForward, GoLeft , GoRight]
 "YOU'VE FOUND THE EXIT!!"
 -}
+
+-- Step 1
+data Move = GoForward | GoLeft | GoRight
+
+data Forest a = FoundExit | Trail a (Forest a) (Forest a) (Forest a) deriving (Show)
+
+-- Step 2
+move :: Num a => (a, Forest a) -> Move -> (a, Forest a)
+move (s, FoundExit) _ = (s, FoundExit)
+move (s, Trail a x _ _) GoLeft = (s - a, x)
+move (s, Trail a _ x _) GoForward = (s - a, x)
+move (s, Trail a _ _ x) GoRight = (s - a, x)
+
+-- Step 3
+testForest :: Forest Int
+testForest =
+  Trail
+    3
+    ( Trail
+        7
+        (Trail 3 FoundExit FoundExit FoundExit)
+        (Trail 4 FoundExit FoundExit FoundExit)
+        (Trail 5 FoundExit FoundExit FoundExit)
+    )
+    ( Trail
+        3
+        (Trail 3 FoundExit FoundExit FoundExit)
+        (Trail 9 FoundExit FoundExit FoundExit)
+        (Trail 5 FoundExit FoundExit FoundExit)
+    )
+    ( Trail
+        5
+        (Trail 3 FoundExit FoundExit FoundExit)
+        (Trail 4 FoundExit FoundExit FoundExit)
+        (Trail 1 FoundExit FoundExit FoundExit)
+    )
+
+-- Step 4
+solveForest' :: (Show a, Ord a, Num a) => Forest a -> [Move] -> (a, Forest a)
+solveForest' forest = foldl move (10, forest)
+
+-- Step 5
+showCurrentChoice :: (Show a, Ord a, Num a) => (a, Forest a) -> String
+showCurrentChoice (s, f)
+  | s <= 0 = "You ran out of stamina and died -.-!"
+  | otherwise = case f of
+    FoundExit -> "YOU'VE FOUND THE EXIT!!"
+    _ -> "You have " ++ show s ++ " stamina, and you're still inside the Forest. Choose a path, brave adventurer: GoLeft, GoRight, or GoForward."
+
+-- Step 6
+solveForest :: (Show a, Ord a, Num a) => Forest a -> [Move] -> String
+solveForest forest moves = showCurrentChoice $ foldl move (10, forest) moves
